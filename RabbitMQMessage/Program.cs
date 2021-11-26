e75a68bb1ef6bc308054be3fe49fae0dd17fc7bd
suhallztrk@gmail.com <suhallztrk@gmail.com>
2021-11-26 17:37:47 +0300

    Add project files.

---
+++ RabbitMQMessage/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SignUp();
+            Console.ReadLine();
+
+        }
+        public static void SignUp()
+        {
+            Console.Write("Kullanıcı adı giriniz :");
+            var factory = new ConnectionFactory()
+            {
+
+                HostName = "localhost",
+            };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello", durable: true, exclusive: false, autoDelete: false, arguments: null);
+                    var body = Encoding.UTF8.GetBytes(Console.ReadLine());
+                    channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+
+
+                }
+            }
+            SignUp();
+        }
+    }
+
+}
